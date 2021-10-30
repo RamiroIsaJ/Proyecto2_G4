ce8e801c4201f8ce7b487470665054fa3f643649
Pablo <paablo.92@hotmail.es>
2021-10-30 14:21:43 -0300

    terminado catalogo

--- js/catalogo.js
+++ js/catalogo.js
@@ -40,15 +40,31 @@ function finSesion() {
 
 cargaInicial();
 
-function crearColumna(producto) {
-    let grilla = document.querySelector("#grilla");
-    grilla.innerHTML += `<div class="col-sm-12 col-md-4 col-lg-3 mb-3">
-      <div class="card">
-          <img src="${producto.url}" class="card-img-top" alt="${producto.nombre}">
-          <div class="card-body">
-            <h5 class="card-title">${producto.nombre}</h5>
-            <p class="card-text">${producto.descripcion}</p>
-          </div>
-        </div>
-  </div>`;
+
+let listaLibros = [];
+
+cargarInicial();
+
+function cargarInicial() {
+  // revisar los datos del localstorage
+  listaLibros = JSON.parse(localStorage.getItem("listaLibrosT")) || [];
+  // dibujar las columas con sus respectivas cards
+  if(listaLibros.length > 0){
+    listaLibros.forEach(itemLibro => {
+        crearColumna(itemLibro);
+      });
   }
+}
+
+function crearColumna(libro) {
+  let grilla = document.querySelector("#grilla");
+  grilla.innerHTML += `<div class="col-sm-12 col-md-4 col-lg-3 mb-3">
+    <div class="card">
+        <img src="${libro.direccion}" class="card-img-top" alt="${libro.nombre}">
+        <div class="card-body">
+          <h5 class="card-title">${libro.autor}</h5>
+          <p class="card-text">${libro.precio}</p>
+        </div>
+      </div>
+</div>`;
+}
