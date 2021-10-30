f7281d34fff9ad90431b9fb23c3316ef70dec2db
Pablo <paablo.92@hotmail.es>
2021-10-30 13:14:19 -0300

    sumo info catalogo js

--- js/catalogo.js
+++ js/catalogo.js
@@ -39,3 +39,16 @@ function finSesion() {
 }
 
 cargaInicial();
+
+function crearColumna(producto) {
+    let grilla = document.querySelector("#grilla");
+    grilla.innerHTML += `<div class="col-sm-12 col-md-4 col-lg-3 mb-3">
+      <div class="card">
+          <img src="${producto.url}" class="card-img-top" alt="${producto.nombre}">
+          <div class="card-body">
+            <h5 class="card-title">${producto.nombre}</h5>
+            <p class="card-text">${producto.descripcion}</p>
+          </div>
+        </div>
+  </div>`;
+  }
