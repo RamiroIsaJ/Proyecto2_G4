3c688cefc43eae7f124ff3b5af1c0d4d55a6a181
Pablo <paablo.92@hotmail.es>
2021-10-31 15:10:44 -0300

    agrego carrito de compras

--- js/catalogo.js
+++ js/catalogo.js
@@ -59,12 +59,56 @@ function cargarInicial() {
 function crearColumna(libro) {
   let grilla = document.querySelector("#grilla");
   grilla.innerHTML += `<div class="col-sm-12 col-md-4 col-lg-3 mb-3">
-    <div class="card">
+    <div class="card text-center">
         <img src="${libro.direccion}" class="card-img-top" alt="${libro.nombre}">
         <div class="card-body">
-          <h5 class="card-title">${libro.autor}</h5>
+          <h5 class="card-title">${libro.nombre}</h5>
           <p class="card-text">${libro.precio}</p>
+          
+          <button type="button" class="btn btn-primary text-center" onclick="agregarCarrito('${libro.nombre}','${libro.precio}')" id="comprar" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+  Comprar
+</button>
+<div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="staticBackdropLabel">Carrito de compras</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Seguir comprando"></button>
+      </div>
+      <div class="modal-body">
+       
+
+      <div class="card text-center">
+        
+        <div id="tarjetaCarrito">
+        </div>
+
+        <div id="totalCarrito">
+        </div>
+     </div>
+
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Seguir comprando</button>
+        <a href="/pages/error.html" class="btn btn-primary" role="button" data-bs-toggle="button">Finalizar compra</a>
+      </div>
+    </div>
+  </div>
+</div>
         </div>
       </div>
 </div>`;
+}
+
+
+window.agregarCarrito = (nombre,precio) => {
+    console.log(nombre,precio)
+
+    let carrito = document.querySelector("#tarjetaCarrito");
+  carrito.innerHTML += `
+          <h5 class="card-title mt-1">${nombre}</h5>
+          <p class="card-text">${precio}</p>
+          <hr>`
+
+         
 }
